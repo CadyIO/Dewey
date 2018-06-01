261d3b4d6bd26de70ff540efd5268ab4879eb4c4
Austin Winstanley <austinwinstanley@gmail.com>
2018-06-01 09:19:27 -0500

    More documentation and added some tests

---
+++ src/Dewey.Test/Dewey/Types/DecimalExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using Dewey.Types;
+using Xunit;
+
+namespace Dewey.Test
+{
+    public class DecimalExtensionsTest
+    {
+        [Fact]
+        public void NumberToCurrencyText() => Assert.Equal("Ten Dollars and No Cents", 10m.NumberToCurrencyText());
+
+        [Fact]
+        public void IsBefore()
+        {
+            Assert.True(5.IsBefore(10));
+            Assert.False(10.IsBefore(5));
+        }
+
+        [Fact]
+        public void IsAfter()
+        {
+            Assert.True(10.IsAfter(5));
+            Assert.False(5.IsAfter(10));
+        }
+
+        [Fact]
+        public void AddDecimal()
+        {
+            // Add two decimals.
+            Assert.Equal(10m, 5m.Add(5m));
+
+            // Chain add decimals.
+            Assert.Equal(15m, 5m.Add(5m).Add(5m));
+        }
+    }
+}
--- src/Dewey/Types/DecimalExtensions.cs
+++ src/Dewey/Types/DecimalExtensions.cs
@@ -37,23 +37,74 @@ namespace Dewey.Types
                 var strFarctionPart = fractionPart.NumberToText();
 
                 wordNumber += (fractionPart == 0 ? " No" : strFarctionPart) + (fractionPart == 1 ? " Cent" : " Cents");
-            } else
+            } else {
                 wordNumber += "No Cents";
+            }
 
             return wordNumber;
         }
 
-        public static bool IsAfter(this decimal value, decimal other) => (value > other);
+        /// <summary>
+        /// Determine whether the first decimal value is before the second decimal value.
+        /// </summary>
+        /// <param name="value">The value to test.</param>
+        /// <param name="other">The value to compare.</param>
+        /// <returns>True if before, false otherwise.</returns>
         public static bool IsBefore(this decimal value, decimal other) => (value < other);
 
+        /// <summary>
+        /// Determine whether the first decimal value is after the second decimal value.
+        /// </summary>
+        /// <param name="value">The value to test.</param>
+        /// <param name="other">The value to compare.</param>
+        /// <returns>True if after, false otherwise.</returns>
+        public static bool IsAfter(this decimal value, decimal other) => (value > other);
+
+        /// <summary>
+        /// Converts the value of the specified object to a 64-bit signed integer.
+        /// </summary>
+        /// <param name="value">The decimal value to convert.</param>
+        /// <returns>A 64-bit signed integer that is equivalent to value, or zero if value is null.</returns>
         public static int ToInt(this decimal value) => Convert.ToInt32(value);
+
+        /// <summary>
+        /// Converts the value of the specified decimal number to an equivalent double-precision floating-point number.
+        /// </summary>
+        /// <param name="value">The decimal value to convert.</param>
+        /// <returns>A double-precision floating-point number that is equivalent to value.</returns>
         public static double ToDouble(this decimal value) => Convert.ToDouble(value);
+
+        /// <summary>
+        /// Converts the value of the specified decimal number to an equivalent 64-bit signed integer.
+        /// </summary>
+        /// <param name="value">The decimal value to convert.</param>
+        /// <returns>
+        /// A value rounded to the nearest 64-bit signed integer. If value is halfway between
+        /// two whole numbers, the even number is returned; that is, 4.5 is converted to
+        /// 4, and 5.5 is converted to 6.</returns>
         public static long ToLong(this decimal value) => Convert.ToInt64(value);
 
+        /// <summary>
+        /// Check if the decimal value is zero.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns>True if zero, false otherwise.</returns>
         public static bool IsZero(this decimal value) => (value == decimal.Zero);
 
+        /// <summary>
+        /// Round the decimal value to the provided number of decimal places.
+        /// </summary>
+        /// <param name="value">The value to round.</param>
+        /// <param name="decimals">The number of decimal places.</param>
+        /// <returns>The rounded value.</returns>
         public static decimal Round(this decimal value, int decimals = 2) => Math.Round(value, decimals);
 
+        /// <summary>
+        /// Add the value of two decimals.
+        /// </summary>
+        /// <param name="value">The decimal value to which to add.</param>
+        /// <param name="arg">The decimal value to add.</param>
+        /// <returns>The sum total of the added values.</returns>
         public static decimal Add(this decimal value, decimal arg) => (value + arg);
         public static decimal Add(this decimal value, int arg) => (value + arg.ToDecimal());
         public static decimal Add(this decimal value, double arg) => (value + arg.ToDecimal());
